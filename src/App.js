0e64d3504b96ef44f3348d8cd0e2cece0be441da
sevi221 <sevilaura221@gmail.com>
2023-05-25 12:46:53 +0200

    Feat styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Layout from "./pages/Layout";
-import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Layout />
-      </BrowserRouter>
+      <Layout />
     </>
   );
 };
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,26 +1,29 @@
 import React from "react";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "../assets/scss/Layout.scss";
 
-const Exercise1 = () => {
-  return <h1>Exercise 1</h1>;
-};
-
-const Exercise2 = () => {
-  return <h1>Exercise 2</h1>;
-};
+const Exercise1 = () => <h1>Hola</h1>;
+const Exercise2 = () => <h1>Adiós</h1>;
 
 const Layout = () => {
   return (
-      <div>
-        <h1 className="title">Página Base</h1>
-        <Link to="/exercise1">
-          <button>Exercise 1</button>
-        </Link>
-        <Link to="/exercise2">
-          <button>Exercise 2</button>
-        </Link>
+    <Router>
+      <div className="container">
+        <h1 className="title">Seleccione una opción:</h1>
+        <div className="button-container">
+          <button>
+            <Link to="/exercise1">Exercise 1</Link>
+          </button>
+          <button>
+            <Link to="/exercise2">Exercise 2</Link>
+          </button>
+        </div>
+        <Routes>
+          <Route path="/exercise1" element={<Exercise1 />} />
+          <Route path="/exercise2" element={<Exercise2 />} />
+        </Routes>
       </div>
+    </Router>
   );
 };
 
